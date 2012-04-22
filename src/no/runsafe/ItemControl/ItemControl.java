aeb6ca026174a6f4071bc5f99e6d9bec8d9bf8d7
Morten Nilsen <morten@runsafe.no>
2012-04-22 19:33:35 +0200

    Change to configuration subsystem

--- src/no/runsafe/ItemControl/ItemControl.java
+++ src/no/runsafe/ItemControl/ItemControl.java
@@ -1,32 +1,30 @@
 package no.runsafe.ItemControl;
 
-import java.io.InputStream;
-
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.configuration.IConfigurationDefaults;
 import no.runsafe.framework.configuration.IConfigurationFile;
 
-public class ItemControl extends RunsafePlugin implements IConfigurationFile, IConfigurationDefaults
+import java.io.InputStream;
+
+public class ItemControl extends RunsafePlugin implements IConfigurationFile
 {
 
 	@Override
 	protected void PluginSetup()
 	{
-        this.addComponent(Globals.class);
+		this.addComponent(Globals.class);
 		this.addComponent(PlayerListener.class);
 		this.addComponent(BlockListener.class);
 	}
-	
+
 	@Override
 	public InputStream getDefaultConfiguration()
 	{
 		return getResource(Constants.defaultConfigurationFile);
 	}
-	
+
 	@Override
-	public String getConfigurationPath() 
+	public String getConfigurationPath()
 	{
 		return Constants.configurationFile;
 	}
-	
 }
