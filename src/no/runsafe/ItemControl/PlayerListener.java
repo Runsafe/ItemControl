d9716abbcf7b900081203d6ed8368734315bb408
Morten Nilsen <morten@runsafe.no>
2013-01-13 16:52:37 +0100

    More debug;

--- src/no/runsafe/ItemControl/PlayerListener.java
+++ src/no/runsafe/ItemControl/PlayerListener.java
@@ -1,17 +1,22 @@
 package no.runsafe.ItemControl;
 
 import no.runsafe.framework.event.player.IPlayerInteractEvent;
+import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.RunsafeWorld;
 import no.runsafe.framework.server.event.player.RunsafePlayerInteractEvent;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import org.bukkit.Material;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import java.util.Map;
 
 public class PlayerListener implements IPlayerInteractEvent
 {
-	public PlayerListener(Globals globals)
+	public PlayerListener(Globals globals, IOutput output)
 	{
 		this.globals = globals;
+		this.console = output;
 	}
 
 	@Override
@@ -26,6 +31,7 @@ public class PlayerListener implements IPlayerInteractEvent
 			&& !globals.itemIsDisabled(theWorld, Material.MOB_SPAWNER.getId())
 			&& this.globals.blockShouldDrop(thePlayer.getWorld(), Material.MOB_SPAWNER.getId()))
 		{
+			dump(item.getRaw());
 			if (globals.createSpawner(event.getPlayer(), event.getBlock().getWorld(), event.getTargetBlock(), item))
 			{
 				if (item.getAmount() > 1)
@@ -40,5 +46,14 @@ public class PlayerListener implements IPlayerInteractEvent
 			event.setCancelled(true);
 	}
 
+	private void dump(ConfigurationSerializable raw)
+	{
+		console.fine(String.format("Dumping instance of %s", raw.getClass().getCanonicalName()));
+		Map<String, Object> values = raw.serialize();
+		for (String key : values.keySet())
+			console.fine(String.format(" - %s: %s", key, values.get(key)));
+	}
+
 	private final Globals globals;
+	private final IOutput console;
 }
