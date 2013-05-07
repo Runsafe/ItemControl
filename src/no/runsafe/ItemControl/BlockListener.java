03d523d042f142634165b918e820be1283a66c7d
Kruithne <kruithne@gmail.com>
2013-05-07 12:41:58 +0100

    Updated to reflect changes to enchantment system

--- src/no/runsafe/ItemControl/BlockListener.java
+++ src/no/runsafe/ItemControl/BlockListener.java
@@ -7,7 +7,8 @@ import no.runsafe.framework.server.RunsafeLocation;
 import no.runsafe.framework.server.RunsafeWorld;
 import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.block.RunsafeSpawner;
-import no.runsafe.framework.server.enchantment.RunsafeEnchantmentWrapper;
+import no.runsafe.framework.server.enchantment.RunsafeEnchantment;
+import no.runsafe.framework.server.enchantment.RunsafeEnchantmentType;
 import no.runsafe.framework.server.event.block.RunsafeBlockBreakEvent;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.player.RunsafePlayer;
@@ -37,7 +38,8 @@ public class BlockListener implements IBlockBreakEvent, IBlockDispense
 		RunsafeItemStack heldItem = thePlayer.getItemInHand();
 		final RunsafeBlock theBlock = event.getBlock();
 
-		if (this.globals.blockShouldDrop(thePlayer.getWorld(), theBlock.getTypeId()) && heldItem.containsEnchantment(new RunsafeEnchantmentWrapper(33)))
+		RunsafeEnchantment silkTouch = new RunsafeEnchantment(RunsafeEnchantmentType.SILK_TOUCH);
+		if (this.globals.blockShouldDrop(thePlayer.getWorld(), theBlock.getTypeId()) && heldItem.containsEnchantment(silkTouch))
 		{
 			try
 			{
