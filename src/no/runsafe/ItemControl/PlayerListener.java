de89f523546ed0dab71d8f651b6281e92d096a7d
Kruithne <kruithne@gmail.com>
2013-06-07 21:50:09 +0100

    Use StringUtils split

--- src/no/runsafe/ItemControl/PlayerListener.java
+++ src/no/runsafe/ItemControl/PlayerListener.java
@@ -13,6 +13,7 @@ import no.runsafe.framework.server.event.player.RunsafePlayerInteractEvent;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.worldguardbridge.WorldGuardInterface;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -109,7 +110,7 @@ public class PlayerListener implements IPlayerInteractEvent, IPlayerDeathEvent, 
 		{
 			if (node.contains("."))
 			{
-				String[] parts = node.split(".");
+				String[] parts = StringUtils.split(".");
 				if (!this.noDeathItemsRegions.containsKey(parts[0]))
 					this.noDeathItemsRegions.put(parts[0], new ArrayList<String>());
 
