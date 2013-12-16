6d734bdf96ce7e9ebe1cbbf82f5893597963b36d
Morten Nilsen <morten@runsafe.no>
2013-12-16 16:30:50 +0100

    renamed upstream

--- src/no/runsafe/ItemControl/Plugin.java
+++ src/no/runsafe/ItemControl/Plugin.java
@@ -6,7 +6,7 @@ import no.runsafe.framework.features.Events;
 public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
-	protected void PluginSetup()
+	protected void pluginSetup()
 	{
 		addComponent(Events.class);
 		this.addComponent(Globals.class);
