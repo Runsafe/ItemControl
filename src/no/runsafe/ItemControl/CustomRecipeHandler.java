71fb582305ef99b4c8730f9bc8c46d65b3766398
Kruithne <kruithne@gmail.com>
2014-02-12 00:26:34 +0000

    Move to using a click event instead, recipes don't work unless we do it in the main class onEnable.

--- src/no/runsafe/ItemControl/CustomRecipeHandler.java
+++ src/no/runsafe/ItemControl/CustomRecipeHandler.java
@@ -1,82 +1,40 @@
 package no.runsafe.ItemControl;
 
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.api.IServer;
 import no.runsafe.framework.api.event.IServerReady;
-import no.runsafe.framework.api.event.inventory.IPrepareCraftItem;
+import no.runsafe.framework.api.event.inventory.IInventoryClick;
 import no.runsafe.framework.api.item.ICustomRecipe;
 import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.minecraft.Item;
-import no.runsafe.framework.minecraft.event.inventory.RunsafePrepareItemCraftEvent;
+import no.runsafe.framework.minecraft.event.inventory.RunsafeInventoryClickEvent;
 import no.runsafe.framework.minecraft.inventory.RunsafeCraftingInventory;
+import no.runsafe.framework.minecraft.inventory.RunsafeInventory;
 import no.runsafe.framework.minecraft.inventory.RunsafeInventoryType;
 import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
-import org.bukkit.Bukkit;
-import org.bukkit.inventory.ShapedRecipe;
 
 import java.util.*;
 
-public class CustomRecipeHandler implements IServerReady, IPrepareCraftItem
+public class CustomRecipeHandler implements IServerReady, IInventoryClick
 {
-	public CustomRecipeHandler(IConsole console, IServer server)
+	public CustomRecipeHandler(IConsole console)
 	{
 		this.console = console;
-		this.server = server;
-
-		Character[] chars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-		keys = Arrays.asList(chars);
 	}
 
 	@Override
 	public void OnServerReady()
 	{
 		recipes.addAll(RunsafePlugin.getPluginAPI(ICustomRecipe.class));
-
-		for (ICustomRecipe recipe : recipes)
-		{
-			ShapedRecipe bukkitRecipe = new ShapedRecipe(Item.Unavailable.Cauldron.getItem().getRaw());
-			HashMap<Item, Character> itemKey = new HashMap<Item, Character>(0);
-			Iterator<Character> keyList = keys.iterator();
-			Map<Integer, RunsafeMeta> recipeMap = recipe.getRecipe();
-			StringBuilder recipeString = new StringBuilder();
-
-			for (int slotID = 1; slotID < 10; slotID++)
-			{
-				Item type = Item.Unavailable.Air;
-				if (recipeMap.containsKey(slotID))
-					type = recipeMap.get(slotID).getItemType();
-
-				if (!itemKey.containsKey(type))
-					itemKey.put(type, keyList.next());
-
-				recipeString.append(itemKey.get(type));
-			}
-
-			console.logInformation("Shape: " + recipeString.toString());
-			bukkitRecipe = bukkitRecipe.shape(recipeString.toString().split("(?<=\\G...)"));
-
-			for (String test : recipeString.toString().split("(?<=\\G...)"))
-				console.logInformation(test);
-
-			for (Map.Entry<Item, Character> node : itemKey.entrySet())
-			{
-				bukkitRecipe = bukkitRecipe.setIngredient(node.getValue(), node.getKey().getType());
-				console.logInformation("Setting ing: %s -> %s", node.getValue(), node.getKey().getName());
-			}
-
-			//server.addRecipe(bukkitRecipe);
-			Bukkit.getServer().addRecipe(bukkitRecipe);
-		}
-
 		console.logInformation("Loaded " + recipes.size() + " custom recipes");
 	}
 
 	@Override
-	public void OnPrepareCraftItem(RunsafePrepareItemCraftEvent event)
+	public void OnInventoryClickEvent(RunsafeInventoryClickEvent event)
 	{
-		RunsafeCraftingInventory inventory = event.getInventory();
-		if (inventory.getType() == RunsafeInventoryType.WORKBENCH)
+		RunsafeInventory rawInventory = event.getInventory();
+		if (rawInventory.getType() == RunsafeInventoryType.WORKBENCH)
 		{
+			RunsafeCraftingInventory inventory = (RunsafeCraftingInventory) rawInventory;
 			List<RunsafeMeta> items = inventory.getMatrix();
 			for (ICustomRecipe recipe : recipes)
 			{
@@ -171,7 +129,5 @@ public class CustomRecipeHandler implements IServerReady, IPrepareCraftItem
 	}
 
 	private final IConsole console;
-	private final IServer server;
-	private final List<Character> keys;
 	private final List<ICustomRecipe> recipes = new ArrayList<ICustomRecipe>(0);
 }
