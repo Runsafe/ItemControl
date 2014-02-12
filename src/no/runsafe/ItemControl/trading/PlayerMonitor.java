cb74d5f621a10cfb9dbfa507cc244650a79a30fd
Kruithne <kruithne@gmail.com>
2014-02-12 05:09:05 +0000

    Debugging.

--- src/no/runsafe/ItemControl/trading/PlayerMonitor.java
+++ src/no/runsafe/ItemControl/trading/PlayerMonitor.java
@@ -1,6 +1,7 @@
 package no.runsafe.ItemControl.trading;
 
 import no.runsafe.framework.api.event.player.IPlayerInteractEntityEvent;
+import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.entity.LivingEntity;
 import no.runsafe.framework.minecraft.entity.RunsafeEntity;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerInteractEntityEvent;
@@ -17,19 +18,29 @@ public class PlayerMonitor implements IPlayerInteractEntityEvent
 	@Override
 	public void OnPlayerInteractEntityEvent(RunsafePlayerInteractEntityEvent event)
 	{
+		IPlayer player = event.getPlayer();
+		player.sendColouredMessage("Entity interact event detected");
+
 		RunsafeEntity entity = event.getRightClicked();
 		if (entity.getEntityType() == LivingEntity.Villager && handler.isTrader(entity))
 		{
+			player.sendColouredMessage("Is villager/trader");
 			RunsafeInventory inventory = handler.getTraderInventory(entity);
 			for (int col = 0; col < 10; col++)
 			{
 				RunsafeMeta firstItem = inventory.getItemInSlot(col);
 				if (firstItem == null)
+				{
+					player.sendColouredMessage("First item is null");
 					return;
+				}
 
 				RunsafeMeta result = inventory.getItemInSlot(col + 9);
 				if (result == null)
+				{
+					player.sendColouredMessage("Result is null");
 					return;
+				}
 
 				VillagerController controller = new VillagerController(entity);
 				controller.clearTrades();
