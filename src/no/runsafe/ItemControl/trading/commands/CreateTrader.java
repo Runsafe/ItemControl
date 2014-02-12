e817e2b6e2a3dd8a9484298ebcd9af1373996426
Kruithne <kruithne@gmail.com>
2014-02-12 02:36:15 +0000

    Test wiping a villager.

--- src/no/runsafe/ItemControl/trading/commands/CreateTrader.java
+++ src/no/runsafe/ItemControl/trading/commands/CreateTrader.java
@@ -1,23 +1,25 @@
 package no.runsafe.ItemControl.trading.commands;
 
-import no.runsafe.ItemControl.trading.TradeNode;
+import net.minecraft.server.v1_7_R1.EntityVillager;
 import no.runsafe.ItemControl.trading.TradingHandler;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.RequiredArgument;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.event.player.IPlayerInteractEntityEvent;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.entity.LivingEntity;
 import no.runsafe.framework.minecraft.entity.RunsafeEntity;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerInteractEntityEvent;
+import org.bukkit.craftbukkit.v1_7_R1.entity.CraftVillager;
+import org.bukkit.entity.Villager;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateTrader extends PlayerCommand implements IPlayerInteractEntityEvent
 {
 	public CreateTrader(TradingHandler handler)
 	{
-		super("createtrader", "Create a trader!", "runsafe.itemcontrol.traders.create", new RequiredArgument("trade"));
+		super("createtrader", "Create a trader!", "runsafe.itemcontrol.traders.create");
 		this.handler = handler;
 	}
 
@@ -25,8 +27,8 @@ public class CreateTrader extends PlayerCommand implements IPlayerInteractEntity
 	public String OnExecute(IPlayer executor, IArgumentList parameters)
 	{
 		String playerName = executor.getName();
-		if (!interactTrack.containsKey(playerName))
-			interactTrack.put(playerName, parameters.get("trade"));
+		if (!interactTrack.contains(playerName))
+			interactTrack.add(playerName);
 
 		return "&eRight-click on a villager to make it a trader!";
 	}
@@ -37,22 +39,20 @@ public class CreateTrader extends PlayerCommand implements IPlayerInteractEntity
 		IPlayer player = event.getPlayer();
 		String playerName = player.getName();
 
-		if (interactTrack.containsKey(playerName))
+		if (interactTrack.contains(playerName))
 		{
 			RunsafeEntity entity = event.getRightClicked();
 			if (entity.getEntityType() == LivingEntity.Villager)
 			{
-				String tradeID = interactTrack.get(playerName);
-				if (handler.isTrader(entity))
-					handler.getTradeNode(entity).setTestValue(tradeID);
-				else
-					handler.setTradeNode(entity, new TradeNode(tradeID));
+				Villager bukkit = (Villager) entity.getRaw();
+				CraftVillager craft = (CraftVillager) bukkit;
+				EntityVillager nms = craft.getHandle();
 
-				player.sendColouredMessage("Villager node: " + handler.getTradeNode(entity).getTestValue());
+				nms.getOffers(null).clear();
 			}
 		}
 	}
 
-	private final HashMap<String, String> interactTrack = new HashMap<String, String>(0);
+	private final List<String> interactTrack = new ArrayList<String>(0);
 	private final TradingHandler handler;
 }
