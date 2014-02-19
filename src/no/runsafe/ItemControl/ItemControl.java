c5214d7a59326697adad39dbbed1354902a3264b
Kruithne <kruithne@gmail.com>
2014-02-19 02:38:23 +0000

    Delete the edit command (for now).

--- src/no/runsafe/ItemControl/ItemControl.java
+++ src/no/runsafe/ItemControl/ItemControl.java
@@ -5,8 +5,6 @@ import no.runsafe.ItemControl.spawners.SpawnerHandler;
 import no.runsafe.ItemControl.spawners.SpawnerMonitor;
 import no.runsafe.ItemControl.trading.*;
 import no.runsafe.ItemControl.trading.commands.CreateTrader;
-import no.runsafe.ItemControl.trading.commands.EditMonitor;
-import no.runsafe.ItemControl.trading.commands.EditTrader;
 import no.runsafe.framework.RunsafeConfigurablePlugin;
 import no.runsafe.framework.api.command.Command;
 import no.runsafe.framework.features.Commands;
@@ -38,13 +36,11 @@ public class ItemControl extends RunsafeConfigurablePlugin
 		addComponent(TradingRepository.class);
 		addComponent(TradingHandler.class);
 
-		addComponent(EditMonitor.class);
 		//addComponent(InventoryMonitor.class);
 
 		Command traderCommand = new Command("traders", "Trader related commands", null);
 		addComponent(traderCommand);
 
 		traderCommand.addSubCommand(getInstance(CreateTrader.class));
-		traderCommand.addSubCommand(getInstance(EditTrader.class));
 	}
 }
