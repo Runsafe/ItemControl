20d84ee96ae1fbcb78ab5c47a51fc442621eb1f3
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-10-23 19:24:05 -0400

    Change tag argument to be optional

--- src/no/runsafe/ItemControl/trading/commands/TagArgument.java
+++ src/no/runsafe/ItemControl/trading/commands/TagArgument.java
@@ -4,13 +4,13 @@ import no.runsafe.ItemControl.trading.ItemTagIDRepository;
 import no.runsafe.framework.api.command.ICommandExecutor;
 import no.runsafe.framework.api.command.argument.ITabComplete;
 import no.runsafe.framework.api.command.argument.IValueExpander;
-import no.runsafe.framework.api.command.argument.RequiredArgument;
+import no.runsafe.framework.api.command.argument.OptionalArgument;
 import no.runsafe.framework.api.player.IPlayer;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class TagArgument extends RequiredArgument implements ITabComplete, IValueExpander
+public class TagArgument extends OptionalArgument implements ITabComplete, IValueExpander
 {
 	public TagArgument(ItemTagIDRepository repository)
 	{
