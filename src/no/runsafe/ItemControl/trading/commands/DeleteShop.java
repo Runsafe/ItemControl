aefecab4ea5c2b7d7d69c3382e2830aaf9adf2c7
DevDeborah <softwaredeveloperdeborah@gmail.com>
2024-09-02 23:32:07 -0400

    Add new command for deleting shops

---
+++ src/no/runsafe/ItemControl/trading/commands/DeleteShop.java
@@ -0,0 +1,25 @@
+package no.runsafe.ItemControl.trading.commands;
+
+import no.runsafe.ItemControl.Globals;
+import no.runsafe.ItemControl.trading.TradingHandler;
+import no.runsafe.framework.api.command.argument.IArgumentList;
+import no.runsafe.framework.api.command.player.PlayerCommand;
+import no.runsafe.framework.api.player.IPlayer;
+
+public class DeleteShop extends PlayerCommand
+{
+	protected DeleteShop(TradingHandler handler)
+	{
+		super("delete", "Deletes a shop", "runsafe.traders.create");
+		this.handler = handler;
+	}
+
+	@Override
+	public String OnExecute(IPlayer executor, IArgumentList parameters)
+	{
+		handler.getDeletingPlayers().add(executor);
+		return Globals.getCommandsShopDeleteMessage();
+	}
+
+	private final TradingHandler handler;
+}
